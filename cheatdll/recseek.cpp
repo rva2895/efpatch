af5238cb8915b647d072d20515c16ad0deebf8d2
rva2895 <rva2895@yandex.ru>
2024-04-17 20:38:28 +0300

    Recseek

---
+++ cheatdll/recseek.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "recseek.h"
+
+TDialogPanel* SeekDialog__SeekDialog(TDialogPanel* this_)
+{
+    TDialogPanel* dialog = (TDialogPanel*)operator_new_internal(sizeof(TDialogPanel));
+    return dialog;
+}
+
+void setRecSeekHooks()
+{
+
+}
