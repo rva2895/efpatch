a61e72fc00b068245602d85e7e267a607c502943
Vasiliy <mrgrenadier2012@yandex.ru>
2024-09-01 23:02:36 +0300

    Add internal functions

--- cheatdll/common.cpp
+++ cheatdll/common.cpp
@@ -184,17 +184,23 @@ int (__cdecl* const tell_internal)(int handle) =
 int (__cdecl* const lseek_internal)(int handle, int distance, int orig) =
     (int (__cdecl* const)(int, int, int))0x0063459E;
 
-__int32 (__cdecl* ftell_internal)(FILE* File) =
-    (__int32 (__cdecl*) (FILE*))0x006361B6;
+FILE* (__cdecl* const fopen_internal)(const char* Filename, const char* Mode) =
+    (FILE* (__cdecl* const)(const char*, const char*))0x006330B1;
 
-int (__cdecl* fseek_internal)(FILE* File, __int32 Offset, int Origin) =
-    (int (__cdecl*) (FILE*, __int32, int))0x00635E6D;
+__int32 (__cdecl* const ftell_internal)(FILE* File) =
+    (__int32 (__cdecl* const) (FILE*))0x006361B6;
 
-size_t (__cdecl* fread_internal)(void* DstBuf, size_t ElementSize, size_t Count, FILE* File) =
-    (size_t (__cdecl*) (void*, size_t, size_t, FILE*))0x006360CE;
+int (__cdecl* const fseek_internal)(FILE* File, __int32 Offset, int Origin) =
+    (int (__cdecl* const) (FILE*, __int32, int))0x00635E6D;
 
-int (__cdecl* fscanf_internal)(FILE* File, const char* Format, ...) =
-    (int (__cdecl*) (FILE*, const char*, ...))0x0063481F;
+size_t (__cdecl* const fread_internal)(void* DstBuf, size_t ElementSize, size_t Count, FILE* File) =
+    (size_t (__cdecl* const) (void*, size_t, size_t, FILE*))0x006360CE;
 
-char* (__cdecl* fgets_internal)(char* Buf, int MaxCount, FILE* File) =
-    (char* (__cdecl*) (char*, int, FILE*))0x0063303A;
+size_t (__cdecl* const fwrite_internal)(const void* Str, size_t Size, size_t Count, FILE* File) =
+    (size_t (__cdecl* const)(const void*, size_t, size_t, FILE*))0x00635EF9;
+
+int (__cdecl* const fscanf_internal)(FILE* File, const char* Format, ...) =
+    (int (__cdecl* const) (FILE*, const char*, ...))0x0063481F;
+
+char* (__cdecl* const fgets_internal)(char* Buf, int MaxCount, FILE* File) =
+    (char* (__cdecl* const) (char*, int, FILE*))0x0063303A;
